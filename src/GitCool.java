d4031182e065a972f5f0da10480e2947711f6bf5
AnastasiiaMikharieva <85961200+AnastasiiaMikharieva@users.noreply.github.com>
2021-07-19 16:42:24 -0400

    Added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,5 +2,8 @@ public class GitCool {
     public static void main(String[] args) {
      //   System.out.println("Learning");
        // System.out.println("I got this!");
+
+
+        //We have added our GitHub account under intelliJ
     }
 }
